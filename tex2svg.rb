74c8d8f9c0832562a0751c37d1abc8972ee4c105
Jacques Distler <distler@golem.ph.utexas.edu>
2019-04-09 23:09:27 -0500

    Bump version

--- tex2svg.rb
+++ tex2svg.rb
@@ -9,7 +9,7 @@ require 'digest'
 class TeX2SVG < Sinatra::Base
   register Sinatra::ConfigFile
 
-  Version = '1.0.5'
+  Version = '1.0.7'
   config_file 'config.yml'
   pdflatex = settings.pdflatex
   pdf2svg = settings.pdf2svg
