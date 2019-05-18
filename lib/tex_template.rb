edd85364826b2252d711edc774b9caeffed224c2
Jacques Distler <distler@golem.ph.utexas.edu>
2019-05-18 18:45:47 -0500

    More STIX Two arrows
    Make the STIX Two arrows the default in the tikxpicture environment
    too.
    Correct some bugs in the LaTeX template.

--- lib/tex_template.rb
+++ lib/tex_template.rb
@@ -4,7 +4,7 @@ module TeXTemplate
 
   def TeXTemplate.tikzpicture(s)
     s, libraries = TeXTemplate.extract_libraries(s)
-    Boilerplate_start + "\n\\usetikzlibrary{#{libraries.join(',') unless libraries.empty?}}\n\\begin{document}\n\\begin{tikzpicture}#{s}\n\\end{tikzpicture}\\end{document}"
+    Boilerplate_start + "\n\\usetikzlibrary{#{libraries.push('arrows.meta','arrows.meta').uniq.join(',')}}\n#{Arrows_start}\n\\begin{document}\n\\begin{tikzpicture}#{s}\n\\end{tikzpicture}\\end{document}"
   end
 
   def TeXTemplate.tikzcd(s)
@@ -18,7 +18,6 @@ module TeXTemplate
 \\usepackage{amsmath}
 \\usepackage{mathrsfs}
 \\usepackage{amsfonts}
-\\usepackage{amssymb}
 \\usepackage{amsthm}
 \\usepackage{mathtools}
 \\usepackage{color}
@@ -111,23 +110,6 @@ module TeXTemplate
 \\def\\root#1#2{\\oldroot #1 \\of{#2}}
 \\renewcommand{\\sqrt}[2][]{\\oldroot #1 \\of{#2}}
 
-% Widecheck
-\\makeatletter
-\\DeclareRobustCommand\\widecheck[1]{{\\mathpalette\\@widecheck{#1}}}
-\\def\\@widecheck#1#2{%
-    \\setbox\\z@\\hbox{\\m@th$#1#2$}%
-    \\setbox\\tw@\\hbox{\\m@th$#1%
-       \\widehat{%
-          \\vrule\\@width\\z@\\@height\\ht\\z@
-          \\vrule\\@height\\z@\\@width\\wd\\z@}$}%
-    \\dp\\tw@-\\ht\\z@
-    \\@tempdima\\ht\\z@ \\advance\\@tempdima2\\ht\\tw@ \\divide\\@tempdima\\thr@@
-    \\setbox\\tw@\\hbox{%
-       \\raise\\@tempdima\\hbox{\\scalebox{1}[-1]{\\lower\\@tempdima\\box
-\\tw@}}}%
-    {\\ooalign{\\box\\tw@ \\cr \\box\\z@}}}
-\\makeatother
-
 % \\mathraisebox{voffset}[height][depth]{something}
 \\makeatletter
 \\NewDocumentCommand\\mathraisebox{moom}{%
@@ -198,12 +180,12 @@ module TeXTemplate
 \\newcommand{\\Union}{\\bigcup}
 \\newcommand{\\Intersection}{\\bigcap}
 \\newcommand{\\Oplus}{\\bigoplus}
-\\newcommand{\\Otimes}{\\bigotimes}
-\\newcommand{\\Wedge}{\\bigwedge}
-\\newcommand{\\Vee}{\\bigvee}
+\\renewcommand{\\Otimes}{\\bigotimes}
+\\renewcommand{\\Wedge}{\\bigwedge}
+\\renewcommand{\\Vee}{\\bigvee}
 \\newcommand{\\coproduct}{\\coprod}
 \\newcommand{\\product}{\\prod}
-\\newcommand{\\closure}{\\overline}
+\\renewcommand{\\closure}{\\overline}
 \\newcommand{\\integral}{\\int}
 \\newcommand{\\doubleintegral}{\\iint}
 \\newcommand{\\tripleintegral}{\\iiint}
@@ -230,6 +212,173 @@ module TeXTemplate
 \\newcommand{\\toggle}[2]{#2}
 BS
 
+  Arrows_start = <<-AS
+%% Use STIX Two arrows. Code to do this from
+%% https://tex.stackexchange.com/questions/396851/stix-arrow-tips-in-tikz-diagrams
+
+%% Arrow declaration:
+\\makeatletter
+\\pgfdeclarearrow{
+  name = stix,
+  parameters = {\\ifpgfarrowharpoon h\\fi},
+  setup code = {
+    \\ifpgfarrowharpoon
+      \\pgfutil@tempdima=\\dimexpr\\pgflinewidth/68
+      \\pgfarrowssettipend{247\\pgfutil@tempdima}
+      \\pgfarrowssetlineend{\\pgfutil@tempdima}
+      \\pgfarrowssetbackend{-81\\pgfutil@tempdima}
+      \\pgfarrowshullpoint{-81\\pgfutil@tempdima}{224\\pgfutil@tempdima}
+      \\pgfarrowshullpoint{-58\\pgfutil@tempdima}{247\\pgfutil@tempdima}
+      \\pgfarrowshullpoint{247\\pgfutil@tempdima}{-34\\pgfutil@tempdima}
+      \\pgfarrowshullpoint{0pt}{-34\\pgfutil@tempdima}
+    \\else\\ifpgfarrowreversed
+      \\pgfutil@tempdima=\\dimexpr\\pgflinewidth/68
+      \\pgfarrowssettipend{0pt}
+      \\pgfarrowssetlineend{-\\pgfutil@tempdima}
+      \\pgfarrowssetbackend{-208\\pgfutil@tempdima}
+      \\pgfarrowsupperhullpoint{-208\\pgfutil@tempdima}{181\\pgfutil@tempdima}
+      \\pgfarrowsupperhullpoint{-188\\pgfutil@tempdima}{201\\pgfutil@tempdima}
+      \\pgfarrowsupperhullpoint{0pt}{34\\pgfutil@tempdima}
+    \\else\\ifdim\\pgfinnerlinewidth>\\z@
+      \\pgfutil@tempdima=\\dimexpr\\pgflinewidth/272
+      \\pgfarrowssettipend{324\\pgfutil@tempdima}
+      \\pgfarrowssetlineend{\\pgfutil@tempdima}
+      \\pgfarrowssetbackend{-73\\pgfutil@tempdima}
+      \\pgfarrowsupperhullpoint{-73\\pgfutil@tempdima}{285\\pgfutil@tempdima}
+      \\pgfarrowsupperhullpoint{-51\\pgfutil@tempdima}{305\\pgfutil@tempdima}
+      \\pgfarrowsupperhullpoint{324\\pgfutil@tempdima}{5\\pgfutil@tempdima}
+    \\else
+      \\pgfutil@tempdima=\\dimexpr\\pgflinewidth/68
+      \\pgfarrowssettipend{192\\pgfutil@tempdima}
+      \\pgfarrowssetlineend{\\pgfutil@tempdima}
+      \\pgfarrowssetbackend{-72\\pgfutil@tempdima}
+      \\pgfarrowsupperhullpoint{-72\\pgfutil@tempdima}{181\\pgfutil@tempdima}
+      \\pgfarrowsupperhullpoint{-51\\pgfutil@tempdima}{201\\pgfutil@tempdima}
+      \\pgfarrowsupperhullpoint{-192\\pgfutil@tempdima}{5\\pgfutil@tempdima}
+    \\fi\\fi\\fi
+  },
+  drawing code = {
+    \\ifpgfarrowharpoon %% harpoon
+      \\pgftransformscale{\\pgflinewidth/68}
+      \\pgfpathmoveto{\\pgfpoint{0}{34}}
+      \\pgfpathcurveto{\\pgfpoint{40}{34}}{\\pgfpoint{44}{50}}{\\pgfpoint{44}{66}}
+      \\pgfpathcurveto{\\pgfpoint{44}{94}}{\\pgfpoint{-22}{156}}{\\pgfpoint{-81}{224}}
+      \\pgfpathlineto{\\pgfpoint{-58}{247}}
+      \\pgfpathcurveto{\\pgfpoint{44}{147}}{\\pgfpoint{117}{66}}{\\pgfpoint{247}{-30}}
+      \\pgfpathlineto{\\pgfpoint{247}{-34}}
+      \\pgfpathlineto{\\pgfpoint{0}{-34}}
+      \\pgfpathclose
+      \\pgfusepathqfill
+    \\else\\ifpgfarrowreversed %% reversed arrowhead
+      \\pgftransformscale{\\pgflinewidth/68}
+      \\pgfpathmoveto{\\pgfpoint{0}{34}}
+      \\pgfpathcurveto{\\pgfpoint{-73}{78}}{\\pgfpoint{-134}{138}}{\\pgfpoint{-188}{201}}
+      \\pgfpathlineto{\\pgfpoint{-208}{181}}
+      \\pgfpathcurveto{\\pgfpoint{-185}{147}}{\\pgfpoint{-152}{118}}{\\pgfpoint{-124}{83}}
+      \\pgfpathcurveto{\\pgfpoint{-111}{65}}{\\pgfpoint{-95}{33}}{\\pgfpoint{-95}{0}}
+      \\pgfpathcurveto{\\pgfpoint{-95}{-33}}{\\pgfpoint{-111}{-65}}{\\pgfpoint{-124}{-83}}
+      \\pgfpathcurveto{\\pgfpoint{-152}{-118}}{\\pgfpoint{-185}{-147}}{\\pgfpoint{-208}{-181}}
+      \\pgfpathlineto{\\pgfpoint{-188}{-201}}
+      \\pgfpathcurveto{\\pgfpoint{-134}{-138}}{\\pgfpoint{-73}{-78}}{\\pgfpoint{0}{-34}}
+      \\pgfpathclose
+      \\pgfusepathqfill
+    \\else\\ifdim\\pgfinnerlinewidth>\\z@ %% double arrow
+      \\pgftransformscale{\\pgflinewidth/272}
+      \\pgfpathmoveto{\\pgfpoint{0}{136}}
+      \\pgfpathcurveto{\\pgfpoint{18}{136}}{\\pgfpoint{27}{151}}{\\pgfpoint{27}{159}}
+      \\pgfpathcurveto{\\pgfpoint{27}{175}}{\\pgfpoint{20}{184}}{\\pgfpoint{3}{202}}
+      \\pgfpathcurveto{\\pgfpoint{-8}{213}}{\\pgfpoint{-48}{256}}{\\pgfpoint{-73}{285}}
+      \\pgfpathlineto{\\pgfpoint{-51}{305}}
+      \\pgfpathcurveto{\\pgfpoint{69}{187}}{\\pgfpoint{198}{100}}{\\pgfpoint{324}{5}}
+      \\pgfpathlineto{\\pgfpoint{324}{-5}}
+      \\pgfpathcurveto{\\pgfpoint{198}{-100}}{\\pgfpoint{69}{-187}}{\\pgfpoint{-51}{-305}}
+      \\pgfpathlineto{\\pgfpoint{-73}{-285}}
+      \\pgfpathcurveto{\\pgfpoint{-48}{-256}}{\\pgfpoint{-8}{-213}}{\\pgfpoint{3}{-202}}
+      \\pgfpathcurveto{\\pgfpoint{20}{-184}}{\\pgfpoint{27}{-175}}{\\pgfpoint{27}{-159}}
+      \\pgfpathcurveto{\\pgfpoint{27}{-151}}{\\pgfpoint{18}{-136}}{\\pgfpoint{0}{-136}}
+      \\pgftransformreset
+      \\pgfpathlineto{\\pgfpoint{0}{-.5*\\pgfinnerlinewidth}}
+      \\pgftransformxshift{208\\pgflinewidth/272}
+      \\pgftransformscale{\\pgfinnerlinewidth/136}
+      \\pgfpathlineto{\\pgfpoint{-61}{-68}}
+      \pgfpathcurveto{\\pgfpoint{-27}{-49}}{\\pgfpoint{0}{-24}}{\\pgfpoint{0}{0}}
+      \\pgfpathcurveto{\\pgfpoint{0}{24}}{\\pgfpoint{-27}{49}}{\\pgfpoint{-61}{68}}
+      \\pgftransformreset
+      \\pgfpathlineto{\\pgfpoint{0}{.5*\\pgfinnerlinewidth}}
+      \\pgfpathclose
+      \\pgfusepathqfill
+    \\else %% normal arrowhead
+      \\pgftransformscale{\\pgflinewidth/68}
+      \\pgfpathmoveto{\\pgfpoint{0}{34}}
+      \\pgfpathcurveto{\\pgfpoint{18}{34}}{\\pgfpoint{26}{44}}{\\pgfpoint{26}{54}}
+      \\pgfpathcurveto{\\pgfpoint{26}{63}}{\\pgfpoint{21}{74}}{\\pgfpoint{12}{83}}
+      \\pgfpathcurveto{\\pgfpoint{-19}{115}}{\\pgfpoint{-48}{148}}{\\pgfpoint{-72}{181}}
+      \\pgfpathlineto{\\pgfpoint{-51}{201}}
+      \\pgfpathcurveto{\\pgfpoint{16}{123}}{\\pgfpoint{94}{47}}{\\pgfpoint{192}{5}}
+      \\pgfpathlineto{\\pgfpoint{192}{-5}}
+      \\pgfpathcurveto{\\pgfpoint{94}{-47}}{\\pgfpoint{16}{-123}}{\\pgfpoint{-51}{-201}}
+      \\pgfpathlineto{\\pgfpoint{-72}{-181}}
+      \\pgfpathcurveto{\\pgfpoint{-48}{-148}}{\\pgfpoint{-19}{-115}}{\\pgfpoint{12}{-83}}
+      \\pgfpathcurveto{\\pgfpoint{21}{-74}}{\\pgfpoint{26}{-63}}{\\pgfpoint{26}{-54}}
+      \\pgfpathcurveto{\\pgfpoint{26}{-44}}{\\pgfpoint{18}{-34}}{\\pgfpoint{0}{-34}}
+      \\pgfpathclose
+      \\pgfusepathqfill
+    \\fi\\fi\\fi
+  }
+}
+\\pgfdeclarearrow{
+  name = stixhooks,
+  parameters = {\\ifpgfarrowharpoon h\\fi},
+  setup code = {
+    \\pgfutil@tempdima=\\dimexpr\\pgflinewidth/68
+    \\pgfarrowssettipend{184\\pgfutil@tempdima}
+    \\pgfarrowssetlineend{\\pgfutil@tempdima}
+    \\pgfarrowssetbackend{-79\\pgfutil@tempdima}
+    \\pgfarrowsupperhullpoint{-79\\pgfutil@tempdima}{307\\pgfutil@tempdima}
+    \\pgfarrowsupperhullpoint{184\\pgfutil@tempdima}{136\\pgfutil@tempdima}
+    \\ifpgfarrowharpoon\\pgfarrowshullpoint{0pt}{-34\\pgfutil@tempdima}\\fi
+  },
+  drawing code = {
+    \\pgftransformscale{\\pgflinewidth/68}
+    \\ifpgfarrowharpoon\\else %% double-sided
+      \\pgfpathmoveto{\\pgfpoint{0}{-34}}
+      \\pgfpathcurveto{\\pgfpoint{61}{-34}}{\\pgfpoint{117}{-68}}{\\pgfpoint{117}{-135}}
+      \\pgfpathcurveto{\\pgfpoint{117}{-197}}{\\pgfpoint{79}{-239}}{\\pgfpoint{18}{-239}}
+      \\pgfpathlineto{\\pgfpoint{-79}{-239}}
+      \\pgfpathlineto{\\pgfpoint{-79}{-307}}
+      \\pgfpathlineto{\\pgfpoint{21}{-307}}
+      \\pgfpathcurveto{\\pgfpoint{99}{-307}}{\\pgfpoint{184}{-245}}{\\pgfpoint{184}{-136}}
+      \\pgfpathcurveto{\\pgfpoint{184}{-20}}{\\pgfpoint{80}{34}}{\\pgfpoint{0}{34}}
+      \\pgfpathclose
+      \\pgfusepathqfill
+    \\fi
+    \\pgfpathmoveto{\\pgfpoint{0}{34}}
+    \\pgfpathcurveto{\\pgfpoint{61}{34}}{\\pgfpoint{117}{68}}{\\pgfpoint{117}{135}}
+    \\pgfpathcurveto{\\pgfpoint{117}{197}}{\\pgfpoint{79}{239}}{\\pgfpoint{18}{239}}
+    \\pgfpathlineto{\\pgfpoint{-79}{239}}
+    \\pgfpathlineto{\\pgfpoint{-79}{307}}
+    \\pgfpathlineto{\\pgfpoint{21}{307}}
+    \\pgfpathcurveto{\\pgfpoint{99}{307}}{\\pgfpoint{184}{245}}{\\pgfpoint{184}{136}}
+    \\pgfpathcurveto{\\pgfpoint{184}{20}}{\\pgfpoint{84}{-34}}{\\pgfpoint{4}{-34}}
+    \\pgfpathclose
+    \\pgfusepathqfill
+  }
+}
+\\makeatother
+
+%% STIX arrows have a line width of exactly 0.68em:
+\\tikzset{every picture/.style={line width=.068em}}
+
+%% Set up arrowheads:
+\\tikzset{>=stix,
+         |/.tip={Bar[width=.403em,line width=.052em]},
+         lefthook/.tip={stixhooks[left]},
+         righthook/.tip={stixhooks[right]},
+         leftharpoon/.tip={>[left]},
+         rightharpoon/.tip={>[right]}
+}
+AS
+
   def TeXTemplate.extract_libraries(s)
     buffer = StringScanner.new(s)
     libraries = []
@@ -242,7 +391,7 @@ BS
         return [out, libraries]
       end
       buffer.scan(/(.*?)\}/)
-      libraries << buffer[1].split(/\s*,\s*/)
+      libraries.concat buffer[1].split(/\s*,\s*/)
     end
     return [out, libraries]
   end
